36360e23ec663b8901e69529e6a66715373dd0eb
Шаймухаметов Ильдар <ildar.shaymukhametov@gmail.com>
2021-08-20 11:40:52 +0500

    add 'else' to ifs

--- csharp/ValidationProject/ProductService.cs
+++ csharp/ValidationProject/ProductService.cs
@@ -16,38 +16,31 @@
             {
                 result = new Response(0, -2, "Missing Name");
             }
-
-            if ("" == (data.Type))
+            else if ("" == (data.Type))
             {
                 result = new Response(0, -2, "Missing Type");
             }
-
-            if ("Lipstick" == data.Type && data.SuggestedPrice > 20 && data.Weight > 0 && data.Weight < 10)
+            else if ("Lipstick" == data.Type && data.SuggestedPrice > 20 && data.Weight > 0 && data.Weight < 10)
             {
                 result = new Response(0, -1, "Error - failed quality check for Queen Range");
             }
-
-            if (data.Weight < 0)
+            else if (data.Weight < 0)
             {
                 result = new Response(0, -3, "Weight error");
             }
-
-            if ("Blusher" == (data.Type) && data.Weight > 10)
+            else if ("Blusher" == (data.Type) && data.Weight > 10)
             {
                 result = new Response(0, -3, "Error - weight too high");
             }
-
-            if ("Lipgloss" == (data.Type) && data.Weight > 20)
+            else if ("Lipgloss" == (data.Type) && data.Weight > 20)
             {
                 result = new Response(0, -3, "Error - weight too high");
             }
-
-            if ("Unknown" == (data.Type))
+            else if ("Unknown" == (data.Type))
             {
                 result = new Response(0, -1, "Unknown product type " + data.Type);
             }
-
-            if (!data.PackagingRecyclable && data.Range == ProductRange.QUEEN)
+            else if (!data.PackagingRecyclable && data.Range == ProductRange.QUEEN)
             {
                 result = new Response(0, -1, "Error - failed quality check for Queen Range");
             }
